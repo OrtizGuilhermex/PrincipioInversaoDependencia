d265fe5a9c0f64bc56e7f706ec310f1a233d4921
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-06 14:51:10 -0300

    fix: Corrige a main

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@
 public class Main {
     public static void main(String[] args) {
         NotificationService emailNotification = new ServicoEmail();
-        RecuperadorDeSenha recuperadorDeSenha = new RecuperadorDeSenha(emailNotification);
-        recuperadorDeSenha.recuperar("");
+        RecuperadorDeSenha recuperadorEmail= new RecuperadorDeSenha(emailNotification);
+        recuperadorEmail.recuperar("usuario@tech.com");
 
         NotificationService SMSNotification = new ServicoSMS();
-        recuperadorDeSenha = new RecuperadorDeSenha(SMSNotification);
-        recuperadorDeSenha.recuperar("");
+        RecuperadorDeSenha recuperadorSMS = new RecuperadorDeSenha(SMSNotification);
+        recuperadorSMS.recuperar("4799999-9999");
 
     }
 }
